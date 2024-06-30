9afe68cb709016163feeadcaeb42a64dc4359920
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 11:09:41 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,9 +3,9 @@ number1 = 10
 number2 = 5
 
 # Adding two nos
-sum = number1 + number2
+Add = number1 + number2
 
 # printing values
-print("Addition of", number1, "and", number2 , "is", sum)
+print("Addition of", number1, "and", number2 , "is", Add)
 
 
