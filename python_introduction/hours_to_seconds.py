a1e6724beeefce7368a081daefa4f0010ce378db
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 12:00:53 +0300

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,1 +1,9 @@
-
+# Python Program to Convert hours to seconds
+ 
+def convert(hours):
+    hours = (hours* 3600)
+    return hours
+     
+# Driver program
+hours = 2
+print('2 hour(s) is', convert(hours), 'seconds.')
