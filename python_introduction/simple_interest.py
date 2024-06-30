9771f54b264b202ff3ff63db937246bf279bfdcc
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 11:17:12 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,13 +4,13 @@
  
  
 def simple_interest(p,t,r):
-    print('The principal is', p)
-    print('The time period is', t)
-    print('The rate of interest is',r)
+    print('The principal =', p)
+    print('The time period =', t)
+    print('The rate of interest =',r)
      
     si = (p * t * r)/100
      
-    print('The Simple Interest is', si)
+    print('The Simple Interest is:', si)
     return si
      
 # Driver code
