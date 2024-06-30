ebd3bcbc41b7594d0bcd09dc606476d11bae63b6
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 15:23:22 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,4 @@
-
-# Python3 program to find simple interest
-
-def interest(P,R,T):simple_interset
+def interest(P,R,T):interest = ( principal * rate * time )
 principal =1000
 rate =0.05
 time =3
@@ -10,3 +7,4 @@ interest = ( principal * rate * time )
      
 print('The simple interest is:', interest)
     
+
