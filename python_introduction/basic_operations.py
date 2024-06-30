c373d594a19709bc528dfaa02a078cca13e142a5
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 11:13:05 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -8,4 +8,14 @@ Add = number1 + number2
 # printing values
 print("Addition of", number1, "and", number2 , "is", Add)
 
+# Subtract two nos
+Sub = number1 - number2
 
+# printing values
+print("Subtraction of", number1, "and", number2 , "is", Sub)
+
+# Subtract two nos
+Mult = number1 * number2
+
+# printing values
+print("Multiplication of", number1, "and", number2 , "is", Sub)
