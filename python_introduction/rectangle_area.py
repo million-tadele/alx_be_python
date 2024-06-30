bc0a7903eb44098910e94bd5950c987f8b5c1146
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 11:38:26 +0300

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,9 +1,9 @@
 # Function to calculate the area using simple multiplication
-def area_simple_multiplication(length, width):
+def Area (length, width):
 	return length * width
 
 # Example usage:
 length = 10
 width = 5
-result = area_simple_multiplication(length, width)
+result = Area (length, width)
 print(f"The area of the rectangle is: {result}")
