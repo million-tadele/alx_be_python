3fc388ef10b428395ca817b0b67bed468ca29689
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-06-30 10:45:22 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,10 +2,18 @@ num1=int(input(10))
 num2=int(input(5))
 num1 = 10
 num2 = 5
+Add = num1 + num2
+print("The Addition of",num1 ,"and",num2 ,"is", Add)
+
+
+
+
+
 sub = num1 - num2
 print("The Subtraction of",num1 ,"and",num2 ,"is", sub)
 
-
+Add = num1 + num2
+print("The Addition of",num1 ,"and",num2 ,"is", Add)
 
 
 
