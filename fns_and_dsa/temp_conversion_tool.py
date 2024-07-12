09c09dec68cd27337d2cf3cd60aeaa372763148e
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-07-12 12:54:11 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@
 
 # Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-def CELSIUS_TO_FAHRENHEIT_FACTOR\s*=\s*9\/5:
+def CELSIUS_TO_FAHRENHEIT_FACTOR = 9\/5:
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
